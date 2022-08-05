4f40452608eed12a351ff7dcd3623952d105907a
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-05 16:43:19 -0700

    adds router and routes to app.js and paths to pages folder files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div className="App">
       <ToastContainer />
-     <h2>hello world</h2>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+     </Routes>
     </div>
   );
 }

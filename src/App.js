8f61ae239b82b547c5ca81823bfdf9c1a9d43c61
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-05 16:38:58 -0700

    adds toastify to app.js and files to pages folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-import './index.css'
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
     <div className="App">
+      <ToastContainer />
      <h2>hello world</h2>
     </div>
   );

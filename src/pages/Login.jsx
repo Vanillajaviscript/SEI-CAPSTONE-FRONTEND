578a10bb4252f9a62b9bfd14733fade22e4731e6
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-05 17:51:05 -0700

    adds sign in MDB forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,7 +21,10 @@ const Login = () => {
   const { email, password } = formState
   const handleSubmit = (e) => {
     
-  }
+  };
+  const onChange = () => {
+
+  };
   return (
     <div
       style={{
@@ -37,9 +40,42 @@ const Login = () => {
         <h4>Sign In</h4>
         <MDBCardBody>
           <MDBValidation onSubmit={handleSubmit} noValidate className="row g-3">
-            
+            <div>
+              <MDBInput
+                label="Email"
+                type="email"
+                value={email}
+                name="email"
+                onChange={onChange}
+                required
+                invalid
+                validation="Please enter your email"
+              />
+            </div>
+            <div>
+              <MDBInput
+                label="Password"
+                type="password"
+                value={password}
+                name="password"
+                onChange={onChange}
+                required
+                invalid
+                validation="Please enter your password"
+              />
+            </div>
+            <div className="col-12">
+              <MDBBtn style={{width: "100%"}} className="mt-2">
+                Login
+              </MDBBtn>
+            </div>
           </MDBValidation>
         </MDBCardBody>
+        <MDBCardFooter>
+          <Link to="/register">
+            <p>Don't have an account? Sign Up</p>
+          </Link>
+        </MDBCardFooter>
       </MDBCard>
     </div>
   )

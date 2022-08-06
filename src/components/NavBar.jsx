928e477afdb6e3896a886d4a12bc3e94c0542397
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-06 13:31:33 -0700

    adds navbar matarial design and links for home

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,18 +10,41 @@ import {
   MDBCollapse,
   MDBNavbarBrand
 } from "mdb-react-ui-kit";
+import '../App.css';;
 
 const NavBar = () => {
   const [navState, setNavState] = useState(false);
 
   return (
-    <MDBNavbar href="/" fixed="top" expand="lg" style={{backgroundColor: "#7994d1"}}>
-      China Dog Rescue
-      <MDBIcon fas icon="dog" size="2x">
+  <MDBNavbar href="/" fixed="top" expand="lg" style={{backgroundColor: "#3a4a6f"}}>
+    <MDBContainer>
+    <MDBNavbarBrand href="/" style={{color: "#c8d2eb"}}>
+      <p>China Dog Rescue</p>
+      <MDBIcon fas icon="dog" size="2x" style={{color: "#c1940c"}}>
       </MDBIcon>
-      <MDBIcon fas icon="bone" size="1x">
+      <MDBIcon fas icon="bone" size="1x" style={{color: "#f3f4f9"}}>
       </MDBIcon>
-    </MDBNavbar>
+    </MDBNavbarBrand>
+    <MDBNavbarToggler
+      type="button"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      onClick={() => setNavState(!navState)}
+      style={{backgroundColor: "#c8d2eb"}}
+      >
+        <MDBIcon icon="bars" fas/>
+    </MDBNavbarToggler>
+    <MDBCollapse show={navState} navbar>
+      <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0">
+        <MDBNavbarItem>
+          <MDBNavbarLink href="/">
+            <p className="nav-link">Home</p>
+          </MDBNavbarLink>
+        </MDBNavbarItem>
+      </MDBNavbarNav>
+    </MDBCollapse>
+    </MDBContainer>
+  </MDBNavbar>
   )
 }
 export default NavBar
